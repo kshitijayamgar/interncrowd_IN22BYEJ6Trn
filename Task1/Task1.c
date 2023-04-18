251ddc387896db4cc4c998a495807e2313ec1c45
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-04-18 15:28:23 +0530

    Top C Projects in 2023 Birthday List
    Top C Projects in 2023Birthday List
    
    In this project, you will create, edit, and display a birthday list of your near and dear ones. You can also provide a search option to find any friend’s birthday using his name. You can also display a list of birthdays coming in a month.

--- Task1/Task1.c
+++ Task1/Task1.c
@@ -1,1 +1,83 @@
+/*
+In this project, create,
+edit and display your loved
+Friend’s/Family’s birthday
+list.
+You can also provide a search
+option to find your
+friend’s birthday by name.
+Youcan also view a list of
+monthly birthdays. Must
+display at least 5 birthdays
+*/
+#include <stdio.h>
+int main() {
+        
+        printf("welcome To The Birthday List Mangement System!!\n");
+        printf("we have the records of 5 friends \n");
+	printf("The list of the names we have is as follow:\n");
+	printf(".Abhijeet\n");
+	printf(".Bhushan\n");
+	printf(".Priya\n");
+	printf(".Sakshi\n");
+	printf(".Maitreyi\n");
+	printf("From the above list choose name and then\n");
+	printf("enter the initial letter of their name to see the birthday details of your friend.\n");
+	printf("NOW!!! enter the initial letter of the name you have choose:");
+                char name;
+                scanf("%c", &name);
 
+	
+        if(name=='A'||name=='B'||name=='P'||name=='S'||name=='M'){
+
+        printf("\n");
+        printf("The details are as follow:\n");
+             switch (name) {
+
+            case 'A':
+	        printf("\n");
+                printf("Name:Abhijeet Joshi\n ");
+		printf("DOB:23/02/1999\n");
+                printf("Email:abhijeet1999@gmail.com\n");
+		 break;
+             
+            case 'B':
+		printf("\n"); 
+                printf("Name:Abhijeet Joshi\n ");
+                printf("DOB:23/12/1992\n");
+                printf("Email:bhushankumbhar1992@gmail.com\n");
+                 break;
+             
+           case 'P':
+		printf("\n");
+		printf("\n");
+                printf("Name:Priya satish mahamuni\n ");
+                printf("DOB:30/10/2002\n");
+                printf("Email:priyamahamuni30@gmail.com\n");
+                 break;
+             
+            case 'S':
+		printf("\n");
+                printf("Name:Sakshi vilas koli\n ");
+                printf("DOB:25/09/1995\n");
+                printf("Email:sakshikoli1995@gmail.com\n");
+                 break;
+             
+            case 'M':
+		printf("\n");
+                printf("Name:Maitreyi anil kulkarni\n ");
+                printf("DOB:1/08/2001\n");
+                printf("Email:maitreyi2001@gmail.com\n");
+                 break;
+            }
+
+	 }
+	     else{
+               
+		        printf("\n");
+                        printf("please enter proper initial letter, the letter you have entered is not from our list!!!\n");
+                       
+                }
+            
+	
+}
